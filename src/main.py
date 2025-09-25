17e3097892d54ee019d8ddb4a0ece61faf14ff02
zojaji.sm <zojaji.sm@gmail.com>
2025-09-25 01:49:03 -0700

    symbol add

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ async def main():
             )
     
  
-    symbols = ["BTC/USDT", "ETH/BTC"]
+    symbols = ["BTC/USDT", "ETH/BTC", "ETH/USDT"]
     timeframes= ["1m", "3m", "5m", "15m", "30m", "1h", "4h", "1d"]
 
     nc = NATS()
